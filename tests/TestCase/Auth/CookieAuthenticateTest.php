194482d409730ce2418b916086b47b3ea74cfabd
nojimage <nojimage@gmail.com>
2017-09-07 18:28:55 +0900

    fixes test

--- tests/TestCase/Auth/CookieAuthenticateTest.php
+++ tests/TestCase/Auth/CookieAuthenticateTest.php
@@ -277,7 +277,7 @@ class CookieAuthenticateTest extends TestCase
      */
     public function testSetLoginTokenToCookieWhenTokenExists()
     {
-        FrozenTime::setTestNow('2017-09-01 12:23:34');
+        FrozenTime::setTestNow('2017-8-01 12:23:34');
         $user = [
             'id' => 1,
             'username' => 'foo',
@@ -303,6 +303,6 @@ class CookieAuthenticateTest extends TestCase
 
         $this->assertSame('series_foo_2', $tokens->last()->series);
         $this->assertSame($decode['token'], $tokens->last()->token);
-        $this->assertSame('2017-10-01T12:23:34+09:00', $tokens->last()->expires->toIso8601String());
+        $this->assertTrue($tokens->last()->expires->eq(new FrozenTime('2017-08-31 12:23:34')), 'default expires is 30days after');
     }
 }
